4896fdf2189c59b45c17da889d5026707311ee23
josielynngriffin <jelynngriffin@gmail.com>
2023-08-06 00:31:50 -0400

    Fix user model imports

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,4 @@
-const { Thought, User } = require('../models/user');
+const { Thought, User } = require('../models');
 
 module.exports = {
     //Get all users, send in json response
--- models/user.js
+++ models/user.js
@@ -41,4 +41,4 @@ userSchema.virtual('friendCount').get(function() {
     return this.friends.length;
 })
 const User = model('User', userSchema);
-module.exports = { User};
+module.exports =  User;
