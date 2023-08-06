4934132c2e2865a8b63ccbd3244dd957dc4a9eab
josielynngriffin <jelynngriffin@gmail.com>
2023-08-05 23:25:54 -0400

    Modify connection

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const {connect, connection} = require('mongoose');
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/departmentsDB', {
+connect('mongodb://127.0.0.1:27017/departmentsDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-module.exports = mongoose.connection;
+module.exports = connection;
